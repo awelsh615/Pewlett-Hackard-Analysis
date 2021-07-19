b68dadfd153a4f31ed182507a5ba44f6b5841440
Amy Welsh <awelsh@davidsonacademy.com>
2021-07-19 18:36:22 -0500

    creating retirement_info.csv

--- Analysis_Projects/Pewlett-Hackard-Analysis/schema.sql
+++ Analysis_Projects/Pewlett-Hackard-Analysis/schema.sql
@@ -56,3 +56,12 @@ CREATE TABLE titles (
 
 SELECT * FROM departments;
 
+-- Retirement eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Look at the retirement_info
+SELECT * FROM retirement_info;
