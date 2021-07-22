bc2e5bd7495cea6a00e242c8dc40350c93d17e07
Amy Welsh <awelsh@davidsonacademy.com>
2021-07-22 15:09:10 -0500

    Deliverable 2 complete

--- Analysis_Projects/Pewlett-Hackard-Analysis/Queries/Employee_Database_Challenge.sql
+++ Analysis_Projects/Pewlett-Hackard-Analysis/Queries/Employee_Database_Challenge.sql
@@ -1,3 +1,4 @@
+-- Deliverable 1
 -- Create retirement_titles table
 DROP TABLE retirement_titles
 
@@ -33,3 +34,26 @@ ORDER BY COUNT (ut.emp_no) DESC;
 
 SELECT *
 FROM retiring_titles;
+
+-- Deliverable 2
+-- Create mentorship_eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibility
+FROM employees as e
+    INNER JOIN dept_emp as de
+    ON (e.emp_no = de.emp_no)
+    INNER JOIN titles as t
+    ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+SELECT *
+FROM mentorship_eligibility;
+
